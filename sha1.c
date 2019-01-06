92593aaec579ac11b139ecacc66a40e5a5d9d632
YJ LEE <steel4154@gmail.com>
2019-01-07 00:09:07 +0900

    Rewrite sha1 codes

--- sha1.c
+++ sha1.c
@@ -3,29 +3,37 @@
 
 #include "sha1.h"
 
-#define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
-#define G(x, y, z) ((x) ^ (y) ^ (z))
-#define H(x, y, z) (((x) & (y)) | ((x) & (z)) | ((y) & (z)))
 #define ROL(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
 
-static void _sha1_iterate(byte_t *msg, word_t *h)
+void sha1_init(struct sha1_ctx *ctx)
+{
+	ctx->h[0] = 0x67452301;
+	ctx->h[1] = 0xefcdab89;
+	ctx->h[2] = 0x98badcfe;
+	ctx->h[3] = 0x10325476;
+	ctx->h[4] = 0xc3d2e1f0;
+
+	ctx->len = 0;
+}
+
+static void _sha1_iterate(struct sha1_ctx *ctx, byte_t *chunk)
 {
 	word_t w[80];
 
 	for (int i = 0; i < 16; ++i) {
 		w[i] = 0;
 		for (int j = 0; j < 4; ++j)
-			w[i] |= (word_t)*(msg + 4 * i + j) << 8 * (3 - j);
+			w[i] |= (word_t)*(chunk + 4 * i + j) << 8 * (3 - j);
 	}
 
 	for (int i = 16; i < 80; ++i)
 		w[i] = ROL(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1);
 
-	word_t a = h[0];
-	word_t b = h[1];
-	word_t c = h[2];
-	word_t d = h[3];
-	word_t e = h[4];
+	word_t a = ctx->h[0];
+	word_t b = ctx->h[1];
+	word_t c = ctx->h[2];
+	word_t d = ctx->h[3];
+	word_t e = ctx->h[4];
 
 	for (int i = 0; i < 80; ++i) {
 		word_t tmp;
@@ -33,20 +41,20 @@ static void _sha1_iterate(byte_t *msg, word_t *h)
 
 		switch (i / 20) {
 		case 0:
-			tmp = F(b, c, d);
+			tmp = (b & c) | ((~b) & d);
 			k = 0x5a827999;
 			break;
 		case 1:
-			tmp = G(b, c, d);
+			tmp = b ^ c ^ d;
 			k = 0x6ed9eba1;
 			break;
 		case 2:
-			tmp = H(b, c, d);
+			tmp = (b & c) | (b & d) | (c & d);
 			k = 0x8f1bbcdc;
 			break;
 		default :
+			tmp = b ^ c ^ d;
 			k = 0xca62c1d6;
-			tmp = G(b, c, d);
 		}
 		tmp += ROL(a, 5) + e + k + w[i];
 		e = d;
@@ -56,40 +64,32 @@ static void _sha1_iterate(byte_t *msg, word_t *h)
 		a = tmp;
 	}
 
-	h[0] += a;
-	h[1] += b;
-	h[2] += c;
-	h[3] += d;
-	h[4] += e;
+	ctx->h[0] += a;
+	ctx->h[1] += b;
+	ctx->h[2] += c;
+	ctx->h[3] += d;
+	ctx->h[4] += e;
 }
 
-void sha1(byte_t *msg, len_t len, byte_t *result)
+void sha1_update(struct sha1_ctx *ctx, byte_t *chunk)
 {
-	len_t bit_len = len * 8;
+	_sha1_iterate(ctx, chunk);
+	ctx->len += 64;
+}
 
-	word_t h[5] = {
-		0x67452301,
-		0xefcdab89,
-		0x98badcfe,
-		0x10325476,
-		0xc3d2e1f0
-	};
-
-	while (len >= 64) {
-		_sha1_iterate(msg, h);
-
-		msg += 64;
-		len -= 64;
-	}
+void sha1_update_last(struct sha1_ctx *ctx, byte_t *chunk, len_t len)
+{
+	ctx->len += len;
+	len_t bit_len = ctx->len * 8;
 
 	byte_t padding[64];
 	memset(padding, 0, sizeof(padding));
 
-	memcpy(padding, msg, len);
+	memcpy(padding, chunk, len);
 	*(padding + len) = 0x80;
 
 	if (len >= 56) {
-		_sha1_iterate(padding, h);
+		_sha1_iterate(ctx, padding);
 		memset(padding, 0, sizeof(padding));
 	}
 
@@ -98,15 +98,19 @@ void sha1(byte_t *msg, len_t len, byte_t *result)
 		*ptr = (bit_len >> 8 * i) & 0xFF;
 		++ptr;
 	}
-	_sha1_iterate(padding, h);
+	_sha1_iterate(ctx, padding);
+}
 
-	if (result == NULL)
-		result = malloc(20);
+void sha1_offline(struct sha1_ctx *ctx, byte_t *msg, len_t len)
+{
+	sha1_init(ctx);
 
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 3; j >= 0; --j) {
-			*result = (h[i] >> 8 * j) & 0xFF;
-			++result;
-		}
+	while (len >= 64) {
+		sha1_update(ctx, msg);
+
+		msg += 64;
+		len -= 64;
 	}
+
+	sha1_update_last(ctx, msg, len);
 }
--- sha1.h
+++ sha1.h
@@ -7,6 +7,19 @@ typedef uint8_t byte_t;
 typedef uint32_t word_t;
 typedef uint64_t len_t;
 
-void sha1(byte_t *msg, len_t len, byte_t *result);
+#define WORD_CNT 5
+
+struct sha1_ctx {
+	word_t h[WORD_CNT];
+	len_t len;
+};
+
+void sha1_init(struct sha1_ctx *ctx);
+
+void sha1_update(struct sha1_ctx *ctx, byte_t *chunk);
+
+void sha1_update_last(struct sha1_ctx *ctx, byte_t *chunk, len_t len);
+
+void sha1_offline(struct sha1_ctx *ctx, byte_t *msg, len_t len);
 
 #endif
